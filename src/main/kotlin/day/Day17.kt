4787919bc7d8cf2618b1992d0078e04fa88018cb
Maaike Wolters <maaike@drbreakalot.nl>
2024-12-18 00:11:24 +0100

    Day 17

--- src/main/kotlin/day/Day17.kt
+++ src/main/kotlin/day/Day17.kt
@@ -29,15 +29,25 @@ object Day17 : Day {
     ) {
         var ip = 0
 
+        fun part2() {
+            while (regA != 0L) {
+                regB = regA % 8 // keep lowest 3 bits: 000000xxx
+                regB = regB xor 2L // flip only middle bit 000000xyx
+                regC = regA shr regB.toInt() // shift regA right by regB bits (0..7)
+                regA = regA / 8 // shift regA right by 3
+                regB = regB xor regC //
+                regB = regB xor 7 // Flip only lowest 3 bits
+                onOutput(regB.toInt() % 8) // keeps only lowest 3 bits
+            }
+        }
+
         fun run() {
             while (ip < program.size) {
                 when (val op = program[ip]) {
                     0 -> adv()
                     1 -> bxl()
                     2 -> bst()
-                    3 -> {
-                        jnz()
-                    }
+                    3 -> jnz()
                     4 -> bxc()
                     5 -> out()
                     6 -> bdv()
@@ -62,7 +72,7 @@ object Day17 : Day {
             ip += 2
         }
 
-        fun executeDivision(): Long = regA / (1.shl(readCombo().toInt()))
+        fun executeDivision(): Long = regA ushr readCombo().toInt()
 
         fun adv() {
             regA = executeDivision()
@@ -139,13 +149,50 @@ object Day17 : Day {
     }
 
     override fun part2(input: List<String>): Result {
-        return NotImplemented
+        val (registers, programLine) = input.split("")
+
+        val (_, regB, regC) = registers.map { it.split(": ").last().toLong() }
+        val program = programLine.first().split(": ").last().split(",").asInts()
+
+        val regA = findRegAWhichMatchesProgram(0..64L, regB, regC, 2, program)
+        return regA.asSuccess()
+    }
+
+    // The programs are made so that each output the program produces only depends on the last 3 bits of regA
+    // Find regA which produces final n of output
+    // with increased values, run function again: increase N by 2, multiply regA by 64
+    // keep doing this if a match is found. If no match is found, increase regA by 1 and try again
+    fun findRegAWhichMatchesProgram(
+        range: LongRange,
+        regB: Long,
+        regC: Long,
+        n: Int,
+        program: List<Int>
+    ): Long? {
+        (range).forEach { i ->
+            //println(i.toString(2).padStart(n * 3, '0'))
+            val output = mutableListOf<Int>()
+            val computer = Computer(i, regB.toLong(), regC.toLong(), program) {
+                output += it
+            }
+            computer.run()
+            //println(output)
+            if (output == program.takeLast(n)) {
+                if (n == program.size) return i
+                val rangeStart = (i shl 6)
+                val match =
+                    findRegAWhichMatchesProgram(rangeStart..(rangeStart + 64), regB, regC, n + 2, program)
+                if (match != null) return match
+            }
+        }
+
+        return null
     }
 
     override fun testData(): Day.TestData {
         return Day.TestData(
             "4,6,3,5,6,3,5,2,1,0",
-            0,
+            117440,
             """
                 Register A: 729
                 Register B: 0
@@ -154,6 +201,14 @@ object Day17 : Day {
                 Program: 0,1,5,4,3,0
                 """
                 .trimIndent()
+                .lines(),
+            """
+                Register A: 2024
+                Register B: 0
+                Register C: 0
+
+                Program: 0,3,5,4,3,0
+            """.trimIndent()
                 .lines()
         )
     }
