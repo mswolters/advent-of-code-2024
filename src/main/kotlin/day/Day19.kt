7a3b78f96c4ff55e0892e96fab7d8cb259ac641e
Maaike Wolters <maaike@drbreakalot.nl>
2024-12-19 23:22:00 +0100

    Day 19

--- src/main/kotlin/day/Day19.kt
+++ src/main/kotlin/day/Day19.kt
@@ -1,21 +1,62 @@
 package day
 
+import findPath
 import split
 
 object Day19 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        val (possibilitiesString, targets) = input.split("")
+        val possibilities = possibilitiesString.first().split(", ")
+
+        return targets.count { target ->
+            findPath(
+                start = "",
+                isEnd = { target == it },
+                edgesForNode = {
+                    possibilities.filter { pos -> target.startsWith(it + pos) }.map { pos -> (it + pos) to 1.0 }
+                }) != null
+        }
+            .asSuccess()
     }
 
     override fun part2(input: List<String>): Result {
-        return NotImplemented
+        val (possibilitiesString, targets) = input.split("")
+        val possibilities = possibilitiesString.first().split(", ")
+
+        // Turns out abusing a pathfinding algorithm to generate all possible words is stupid
+        // Turns out generating all possible words is stupid in the first place
+
+        val map = mutableMapOf<String, Long>()
+        return targets
+            .sumOf { findNumberOfWaysWordCanBeFormedFrom(it, possibilities, map) }
+            .asSuccess()
+    }
+
+    fun findNumberOfWaysWordCanBeFormedFrom(
+        word: String,
+        possibilities: List<String>,
+        memoization: MutableMap<String, Long>
+    ): Long = memoization.getOrPut(word) {
+        if (word.isEmpty()) return@getOrPut 1L
+
+        return@getOrPut possibilities.filter { word.endsWith(it) }
+            .sumOf { findNumberOfWaysWordCanBeFormedFrom(word.removeSuffix(it), possibilities, memoization) }
     }
 
     override fun testData(): Day.TestData {
         return Day.TestData(
-            0,
-            0,
-            """"""
+            6,
+            16,
+            """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
                 .lines()
         )
     }
