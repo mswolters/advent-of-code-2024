d904d727bef157b3db26e3ddd15b795c6e1f8ae9
Maaike Wolters <maaike@drbreakalot.nl>
2024-12-22 19:26:42 +0100

    Day 22 part 1

--- src/main/kotlin/day/Day22.kt
+++ src/main/kotlin/day/Day22.kt
@@ -2,8 +2,19 @@ package day
 
 object Day22 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        return input.asSequence().map { it.toLong() }
+            .map { i ->
+                (0..<2000).fold(i) { j, _ ->
+                    j.let { it.mix(it * 64).prune() }
+                        .let { it.mix(it / 32).prune() }
+                        .let { it.mix(it * 2048).prune() }
+                }
+            }.sum().asSuccess()
     }
+
+    fun Long.mix(i: Long): Long = xor(i)
+
+    fun Long.prune() = and(0b1111_1111_1111_1111_1111_1111)
 
     override fun part2(input: List<String>): Result {
         return NotImplemented
@@ -11,9 +22,12 @@ object Day22 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            37327623,
             0,
-            0,
-            """"""
+            """1
+10
+100
+2024"""
                 .lines()
         )
     }
